cc5a7fdc0d0c92f9ff4139d12ed9af008a29aa1c
ashim <ashimgautam01@gmail.com>
2024-09-18 12:30:08 +0545

    documentation

--- model/service.js
+++ model/service.js
@@ -1,10 +1,9 @@
-
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const serviceSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    description: { type: String, required: true },
-    price: { type: Number, required: true, min: 0 },
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true, min: 0 },
 });
 
-module.exports = mongoose.model('Service', serviceSchema);
+module.exports = mongoose.model("Service", serviceSchema);
